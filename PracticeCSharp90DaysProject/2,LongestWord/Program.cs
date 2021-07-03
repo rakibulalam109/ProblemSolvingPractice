742324d2a13c8753f8bbd3d36c8ae47a61b6cbc1
rakibulalam109 <rakibulalam109@gmail.com>
2021-07-03 18:10:26 +0600

    Day 2 added

---
+++ PracticeCSharp90DaysProject/2,LongestWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2_LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            
+            int topLength = 0;
+            string store = string.Empty;
+
+            foreach (var item in input)
+            {
+                int newItem = item.Length;
+
+                if (newItem > topLength)
+                {
+                    store = item;
+                    topLength = store.Length;
+                }
+                
+
+            }
+            Console.WriteLine(store);
+        }
+    }
+}
